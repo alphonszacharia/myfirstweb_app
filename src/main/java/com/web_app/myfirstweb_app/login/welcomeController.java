184fab6fc924cb2fe4c28f8524f7cc8d65019449
alphonszacharia <alphonszacharia@gmail.com>
2026-08-06 00:04:40 +0530

    login

---
+++ src/main/java/com/web_app/myfirstweb_app/login/welcomeController.java
@@ -0,0 +1,18 @@
+package com.web_app.myfirstweb_app.login;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.ModelMap;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.SessionAttributes;
+
+@Controller
+@SessionAttributes("name")
+public class welcomeController {
+
+
+        @RequestMapping(value = "/",method = RequestMethod.GET)
+        public String sayHelloLoginJsp(ModelMap model) {
+            model.put("name","alphons");
+            return"welcome";
+        }
+}
